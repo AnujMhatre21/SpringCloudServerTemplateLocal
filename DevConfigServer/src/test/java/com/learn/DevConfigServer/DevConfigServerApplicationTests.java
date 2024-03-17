065381eaf6789030ad28ddb17a235c2b8ccc6f02
AnujMhatre <anujmhatre21@gmail.com>
2024-03-17 13:31:05 +0530

    Final Configuration

---
+++ DevConfigServer/src/test/java/com/learn/DevConfigServer/DevConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.DevConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
